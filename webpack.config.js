5cb858b340a58903dc25d8c8bd887e4edd94e52e
Yana <zzizzu@ukr.net>
2019-11-03 23:47:47 +0200

    delete dev mode from config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
